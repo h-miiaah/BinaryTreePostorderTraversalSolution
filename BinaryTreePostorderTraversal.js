bc900426c0c23df6bc2daf216188c618e51316c8
h-miiaah <haim.miiaah5@gmail.com>
2022-02-13 22:22:01 -0500

    comments

--- BinaryTreePostorderTraversal.js
+++ BinaryTreePostorderTraversal.js
@@ -32,6 +32,7 @@
 - We first push the root node into the stack.
 - Then we start a while loop with the condition stack.length !== 0.
 - Inside the loop, we pop the last element from the stack and push it into the result array.
+- After that, we check if the popped element has a left child and push it into the stack.
 
 
 */
